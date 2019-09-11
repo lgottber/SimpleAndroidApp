7ba1d1ff94975f22cf786e1a339922e00dd46eb0
Lauren Gottberg <lauren.gottberg@pendo.io>
2019-09-11 10:29:12 -0400

    updating with the new sdk and setup

--- app/src/main/java/com/example/sampleandroid/App.java
+++ app/src/main/java/com/example/sampleandroid/App.java
@@ -2,35 +2,29 @@ package com.example.sampleandroid;
 import sdk.pendo.io.*;
 import android.app.Application;
 
+//package com.example.sdktest;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
-public class App extends Application {
-    String appKey = "";
-    String visitorId = "Duckies";
-    String accountId = "Ducks";
+import sdk.pendo.io.Pendo;
+
+public class App extends android.app.Application {
     @Override
-    public void onCreate(){
-        Pendo.PendoInitParams pendoParams = new Pendo.PendoInitParams();
-        pendoParams.setVisitorId(visitorId);
-        pendoParams.setAccountId(accountId);
+    public void onCreate() {
+        super.onCreate();
 
-//send Visitor Level Data
-        Map<String, String> userData = new HashMap<>();
-        userData.put("age", "27");
-        userData.put("country", "USA");
-        pendoParams.setUserData(userData);
-
-//send Account Level Data
-        Map<String, String> accountData = new HashMap<>();
-        accountData.put("Tier", "1");
-        accountData.put("Size", "Enterprise");
-        pendoParams.setAccountData(accountData);
+        Random rand = new Random();
+        int visitor = rand.nextInt();
+        String key = "eyJhbGciOiJSUzI1NiIsImtpZCI6IiIsInR5cCI6IkpXVCJ9.eyJkYXRhY2VudGVyIjoidXMiLCJrZXkiOiJjYjM5YjJjNzExOWUxY2RjYTk0ZDAzNGYyNThmMzc0NWMxMTVhZThlMDhhNWFiMDI2OWJjMjgzMGVjMGEyNmRlZGU1MzA1Nzc0MDk4ZWE0MWZjY2UxZjU3MWU1MjM2YmI0MWMzMDgxNWE0MzQ1YmQ4M2E0MDkzMTJlYjA3YjM1ZTNmZTgzZDIwZTVhODk4MTkyNTEzYTRkYzkxNzc5ZjNiLmEyZDZlN2I4ZTI2ODcyYzFlMjhiN2QzNmZmYTJhZjQ0LmVmYWZjNTI1OGZkYmZlMDZkNjg0MzY5MWEyOWE3NDNkZjQyNTE3ZDI0YTRhODYxODFjNmZkODYzMGEzZDBmOTUifQ.KHmog2QYSeBlniztkSf5UHfHHpSAt7bsSPSULoROXHHxIGl6IKWOUNMc9NoRuj0o9YYlQEB7yqnDQg8LjnMTIE01jiYi8wzanrNXAxSFDDAMM2xqxNRlHKEFTzxFwRyelydOzJ5DECWJPMhphJcsc3kHq-11KRY6q9dC4vihi54";
 
 
+        Pendo.PendoInitParams initParams = new Pendo.PendoInitParams().setVisitorId("Nikola Tesla" + visitor).setAccountId("BetterThanYou");
+        initParams.setPendoOptions(new Pendo.PendoOptions.Builder().setEnvironmentDebugOnly("DEV").build());
         Pendo.initSDK(
                 this,
-                appKey,
-                pendoParams);   // call initSDK with initParams as a fourth parameter (this can be `null`).
+                key,
+                initParams);
     }
 }
